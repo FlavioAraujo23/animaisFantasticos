7fc29a67bcbba52939b6c5236249de96361b3830
FlavioAraujo23 <flaviolopes1027@gmail.com>
2023-10-04 21:26:14 -0300

    tabnav transformado em class

--- js/modules/activeTab.js
+++ js/modules/activeTab.js
@@ -1,24 +1,31 @@
-export default function initTabnav(){
-  const tabMenu = document.querySelectorAll('[data-tab="menu"] li');
-  const tabContent = document.querySelectorAll('[data-tab="content"] section');
-  
-  tabContent[0].classList.add('ativo') // adicionando a classe ao primeiro item do site para que ao entrar ele ja carregue com as info, e a troca de informaçoes so ocorrera ao click na img
-  
-  if (tabMenu.length && tabContent.length){ // o if verifica se as section do tabMenu existem, e tambem se ha algo na tabcontent, caso nao o retorno sera 0 ou seja false e codigo nao ocorrera
-  
-    function activeTab(index) {
-      tabContent.forEach((section) => {
-        section.classList.remove('ativo')
-      }) // funcao que ativa a class em somente um item
-      const direcao = tabContent[index].dataset.anime
-      tabContent[index].classList.add('ativo',direcao);
+export default class TabNav {
+  constructor(menu, content) {
+    this.tabMenu = document.querySelectorAll(menu);
+    this.tabContent = document.querySelectorAll(content);
+    this.activeClass = 'ativo';
+  }
+  // ativa a tab de acordo com o index da mesma
+
+  activeTab(index) {
+    this.tabContent.forEach((section) => {
+      section.classList.remove(this.activeClass);
+    }); // funcao que ativa a class em somente um item
+    const direcao = this.tabContent[index].dataset.anime;
+    this.tabContent[index].classList.add(this.activeClass, direcao);
+  }
+  // adiciona os eventos as tabs
+
+  addTabNavEvent() {
+    this.tabMenu.forEach((itemMenu, index) => {
+      itemMenu.addEventListener('click', () => this.activeTab(index));
+    });
+  }
+
+  init() {
+    if (this.tabMenu.length && this.tabContent.length) {
+      // ativar primeiro item
+      this.activeTab(0);
+      this.addTabNavEvent();
     }
-  
-    tabMenu.forEach((itemMenu, index) => {
-    itemMenu.addEventListener('click', () => {
-      activeTab(index)
-    })   // qunado executado o click ativa a arrow function, que ativa dentro dela a funcao de adicionar a classe ativo
-    })
   }
-  }
- 
+}
